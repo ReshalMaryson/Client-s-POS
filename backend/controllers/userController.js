c8e1ba7c00cacce9d82a08aed7b737f8b4e978f1
Reshal Maryson <reshalmaryson000@gmail.com>
2026-07-26 03:54:15 -0700

    Remove debug console logs in updateUser function
    Removed console logs for parameter ID and its type.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -198,8 +198,7 @@ exports.deleteAUser = async (req, res) => {
 exports.updateUser = async (req, res) => {
   try {
     const Id = req.params.id;
-    console.log("PARAM ID:", Id);
-    console.log("TYPE:", typeof Id);
+   
     if (!mongoose.Types.ObjectId.isValid(Id)) {
       return res.status(400).json({ message: "invalid Id" });
     }
